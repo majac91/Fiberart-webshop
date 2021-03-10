48d94dd6316bd57701ad3c72e8ac9311822683ca
Maja <majacvetkovic@outlook.com>
2021-03-10 17:17:21 +0100

    useListOnScreen hook for using IO on multiple refs and ProductList component refactor

--- src/hooks/useOnIntersection.js
+++ src/hooks/useOnIntersection.js
@@ -34,3 +34,29 @@ export function useElementOnScreen(ref, imgSrc) {
 
   return isIntersecting;
 }
+
+export function useListOnScreen(refList) {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  useEffect(() => {
+    refList.current.forEach((ref) => {
+      const options = { treshold: 1 };
+
+      const callback = (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsIntersecting(true);
+          }
+        });
+      };
+
+      const observer = new IntersectionObserver(callback, options);
+      observer.observe(ref);
+    });
+
+    // const currentRef = ref.current;
+    // return () => observer.unobserve(currentRef);
+  }, [refList]);
+
+  return isIntersecting;
+}
--- src/pages/Shop/components/ProductsList/ProductList.js
+++ src/pages/Shop/components/ProductsList/ProductList.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { useElementOnScreen } from "../../../../hooks/useOnIntersection";
+import { useListOnScreen } from "../../../../hooks/useOnIntersection";
 import { Link } from "react-router-dom";
 import { useProductList } from "../../../../firebase/api";
 import Loader from "../../../../components/Loader/Loader";
@@ -8,49 +8,64 @@ const cx = require("classnames");
 
 const Products = () => {
   const products = useProductList();
-  const placeholder = useRef();
-  const isOnScreen = useElementOnScreen(placeholder, null);
+  // const placeholder = useRef();
+  const imgRefs = useRef([]);
+
+  const isOnScreen = useListOnScreen(imgRefs);
+  console.log(isOnScreen);
+
   const placeholderStyle = cx("placeholder", { hidePlaceholder: isOnScreen });
+
+  const createElements = () => {
+    let elements = [];
+    if (products) {
+      Object.keys(products).map((key) => {
+        let product = products[key];
+        return elements.push(
+          <div
+            ref={(ref) => imgRefs.current.push(ref)}
+            key={key}
+            className={productStyles.imgWrapper}
+          >
+            <Link
+              to={`product/${key}`}
+              className={productStyles.imgLink}
+            ></Link>
+            <img
+              alt={product.name}
+              className={productStyles.productImg}
+              src={product.image}
+            ></img>
+            <div className={productStyles.captionWrapper}>
+              <div className={productStyles.caption}>{product.name}</div>
+              <div className={productStyles.price}>{product.price}</div>
+
+              <Link className={productStyles.button} to={`product/${key}`}>
+                view item
+              </Link>
+            </div>
+          </div>
+        );
+      });
+    }
+    return elements;
+  };
 
   return (
     <>
-      {isOnScreen ? (
+      <div className={`${productStyles.container} container`}>
+        {createElements()}
+      </div>
+
+      {/* {isOnScreen ? (
         <div className={`${productStyles.container} container`}>
-          {products &&
-            Object.keys(products).map((key) => {
-              let product = products[key];
-
-              return (
-                <div key={key} className={productStyles.imgWrapper}>
-                  <Link
-                    to={`product/${key}`}
-                    className={productStyles.imgLink}
-                  ></Link>
-                  <img
-                    alt={product.name}
-                    className={productStyles.productImg}
-                    src={product.image}
-                  ></img>
-                  <div className={productStyles.captionWrapper}>
-                    <div className={productStyles.caption}>{product.name}</div>
-                    <div className={productStyles.price}>{product.price}</div>
-
-                    <Link
-                      className={productStyles.button}
-                      to={`product/${key}`}
-                    >
-                      view item
-                    </Link>
-                  </div>
-                </div>
-              );
-            })}
+          {createElements()}
         </div>
       ) : (
-        <div className={placeholderStyle} ref={placeholder}>
+        <div className={placeholderStyle}>
           <Loader />
         </div>
-      )}
+      )} */}
     </>
   );
 };
